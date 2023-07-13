d6bbecc94db49deb1d57a78eb022613943b4ec54
Benmuiruri <ben.muiruri13@gmail.com>
2023-07-13 12:18:07 +0300

    Fix: Refactor the code after Reek review

--- job_scraper.rb
+++ job_scraper.rb
@@ -1,59 +1,103 @@
 require 'httparty'
 require 'nokogiri'
 
-KEYWORDS = ["React", "Ruby", "Rails"]
+KEYWORDS = ["Ruby", "Rails"]
+JOB_POSTINGS_FILE = "job_postings.txt"
+PAGES_TO_SCRAPE = 5
 
-def fetch_jobs(page)
-  url = "https://news.ycombinator.com/item?id=35773707&p=#{page}"
-  unparsed_page = HTTParty.get(url)
-  Nokogiri::HTML(unparsed_page)
-end
+# PageRetriever is responsible for fetching an HTML document from a given URL
+class PageRetriever
+  def self.fetch(url)
+    response = HTTParty.get(url)
 
-def filtered_jobs(document, keywords)
-  job_posts = document.css("tr.athing.comtr")
-
-  job_posts.select do |job_post|
-    text = job_post.text
-    keywords.any? { |keyword| text.include?(keyword) }
+    if response.success?
+      Nokogiri::HTML(response.body)
+    else
+      raise "Unable to fetch the page, response code: #{response.code}"
+    end
+  rescue StandardError => error
+    puts "Error occurred while fetching jobs: #{error.message}"
   end
 end
 
-def clean_text(text)
-  text.gsub(/\s+/, ' ').strip
-end
+# JobScraper is responsible for scraping job postings from a website
+class JobScraper
+  # TextHelper is responsible for cleaning text and HTML
+  class TextHelper
+    def self.clean_text(text)
+      text.gsub(/\s+/, ' ').strip
+    end
 
-def extract_job_details(comment)
-  {
-    title: clean_text(comment.css('.hnuser').text),
-    time: clean_text(comment.css('.age a').text),
-    details: clean_text(comment.css('.commtext').inner_html.gsub(/<[^>]*>/, ''))
-  }
-end
-
-def formatted_job_postings(job_postings)
-  output = "Found #{job_postings.count} job postings\n"
-  job_postings.each_with_index do |posting, index|
-    output << "Job posting #{index + 1}: #{posting[:title]} | #{posting[:time]}\n#{posting[:details]}\n\n"
+    def self.clean_html(html)
+      html.gsub(/<[^>]*>/, '')
+    end
   end
-  output
-end
 
-def save_jobs(jobs)
-  File.open("job_postings.txt", "w") do |file|
-    file.write(jobs)
+  # JobFilter is responsible for filtering job postings based on given keywords
+  class JobFilter
+    def self.filter(document)
+      return [] unless document
+
+      document.css("tr.athing.comtr").reduce([]) do |acc, job_post|
+        acc << job_post if keyword_present_in_text?(job_post.text)
+        acc
+      end
+    end
+
+    def self.keyword_present_in_text?(text)
+      KEYWORDS.any? { |keyword| text.include?(keyword) }
+    end
+  end
+
+  # JobDetailsExtractor is responsible for extracting details from job postings
+  class JobDetailsExtractor
+    def self.extract(comment)
+      {
+        title: TextHelper.clean_text(comment.css('.hnuser').text),
+        time: TextHelper.clean_text(comment.css('.age a').text),
+        details: TextHelper.clean_text(TextHelper.clean_html(comment.css('.commtext').inner_html))
+      }
+    end
+  end
+
+  # JobFormatter is responsible for formatting job postings for output
+  class JobFormatter
+    def self.format(job_postings)
+      output = ["Found #{job_postings.count} job postings"]
+      job_postings.each_with_index do |posting, index|
+        output << "Job posting #{index + 1}: #{posting[:title]} | #{posting[:time]}\n#{posting[:details]}\n"
+      end
+      output.join("\n")
+    end
+  end
+
+  def initialize
+    @all_jobs = []
+  end
+
+  def scrape_jobs
+    retrieve_all_jobs
+    cleaned_jobs = @all_jobs.map { |job| JobDetailsExtractor.extract(job) }
+    save_jobs(JobFormatter.format(cleaned_jobs))
+  end
+
+  private
+
+  def retrieve_all_jobs
+    puts "Scraping #{PAGES_TO_SCRAPE} pages for jobs with keywords: #{KEYWORDS.join(', ')}"
+    (1..PAGES_TO_SCRAPE).each do |page|
+      url = "https://news.ycombinator.com/item?id=36573871&p=#{page}"
+      document = PageRetriever.fetch(url)
+      @all_jobs += JobFilter.filter(document)
+    end
+  end
+
+  def save_jobs(jobs, file = JOB_POSTINGS_FILE)
+    File.open(file, "w") { |file| file.write(jobs) }
+    puts "Found #{all_jobs.count} jobs and saved them to #{JOB_POSTINGS_FILE}"
+  rescue StandardError => error
+    puts "Error occurred while saving jobs: #{error.message}"
   end
 end
 
-# Scrape the first 3 pages
-pages_to_scrape = 3
-all_jobs = []
-
-(1..pages_to_scrape).each do |page|
-  document = fetch_jobs(page)
-  jobs = filtered_jobs(document, KEYWORDS)
-  all_jobs += jobs
-end
-
-cleaned_jobs = all_jobs.map { |job| extract_job_details(job) }
-formatted_jobs = formatted_job_postings(cleaned_jobs)
-save_jobs(formatted_jobs)
+JobScraper.new.scrape_jobs
